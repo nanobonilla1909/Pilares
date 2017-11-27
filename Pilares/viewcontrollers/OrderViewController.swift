0679b4387bf7e46c2ec8550f3f804bce9221fcf9
Mariano Bonilla <marianobonilla@MacBook-Pro-de-Mariano.local>
2017-11-27 16:34:09 -0300

    Agregado el Order View Controller sin TAble View

---
+++ Pilares/viewcontrollers/OrderViewController.swift
@@ -0,0 +1,21 @@
+//
+//  OrderViewController.swift
+//  Pilares
+//
+//  Created by Mariano Bonilla on 27/11/17.
+//  Copyright © 2017 Mariano Bonilla. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
